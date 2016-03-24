d505b76c994bba194ee3537b4dd6391648114dbd
Steve Grunwell <stevegrunwell@gmail.com>
2016-03-23 22:15:27 -0400

    Add the main plugin file

---
+++ mcavoy.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: McAvoy
+ * Plugin URI:  https://github.com/stevegrunwell/mcavoy
+ * Description: Discover what visitors are searching for on your WordPress site.
+ * Version:     0.1.0
+ * Author:      Steve Grunwell
+ * Author URI:  https://stevegrunwell.com
+ * License:     MIT
+ * Text Domain: mcavoy
+ * Domain Path: /languages
+ *
+ * @package McAvoy
+ * @author  Steve Grunwell
+ */
+
+namespace McAvoy;
+
+require_once __DIR__ . '/inc/database.php';
+require_once __DIR__ . '/inc/logger.php';
